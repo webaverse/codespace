f3b6886b4614dd10c97bc9948f2d8d917e0d927c
A Goblin King <462380+AGoblinKing@users.noreply.github.com>
2021-08-17 22:24:00 +0000

    sets env variables from the devcontainer

--- init.sh
+++ init.sh
@@ -5,8 +5,7 @@ echo "Cert Time"
 rm -rf certs
 mkdir certs
 
-openssl genrsa > ./certs/privkey.pem
-openssl req -new -x509 -subj '/CN=webaverse.com/O=Webaverse/C=US' -key ./certs/privkey.pem > ./certs/fullchain.pem
+openssl genrsa > /workspaces/codespace/certs/privkey.pem
+openssl req -new -x509 -subj '/CN=webaverse.com/O=Webaverse/C=US' -key /workspaces/codespace/certs/privkey.pem > /workspaces/codespace/certs/fullchain.pem
 
-cp -rf ./certs ./node_modules/dialog/certs
 echo "You are ready!"
