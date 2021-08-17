9a39aea80290fe66287c1ccbe1c7f5e7322b2b97
A Goblin King <462380+AGoblinKing@users.noreply.github.com>
2021-08-17 19:43:06 +0000

    Start up both dialog and webaverse in parallel. Initialize the certs in dialog.

--- init.sh
+++ init.sh
@@ -8,4 +8,5 @@ mkdir certs
 openssl genrsa > ./certs/privkey.pem
 openssl req -new -x509 -subj '/CN=webaverse.com/O=Webaverse/C=US' -key ./certs/privkey.pem > ./certs/fullchain.pem
 
+cp -rf ./certs ./node_modules/dialog/certs
 echo "You are ready!"
