dd6dfeb657deab57c1d9df3cad4fd710880357dc
A Goblin King <462380+AGoblinKing@users.noreply.github.com>
2021-08-23 23:02:31 +0000

    move the node_modules stashed in webaverse to this workspace

--- create.sh
+++ create.sh
@@ -7,6 +7,6 @@ mkdir certs
 openssl genrsa > /workspaces/codespace/certs/privkey.pem
 openssl req -new -x509 -subj '/CN=webaverse.com/O=Webaverse/C=US' -key /workspaces/codespace/certs/privkey.pem > /workspaces/codespace/certs/fullchain.pem
 
-mv /tmp/codespace/node_modules /workspaces/codespace/node_modules
+mv /webaverse/node_modules /workspaces/codespace/node_modules
 
 echo "You are ready!"
